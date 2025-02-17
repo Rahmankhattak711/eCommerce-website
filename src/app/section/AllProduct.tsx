142b0a3283baaecdbbe5734432b9287552838aa3
rahmankhattak711 <rahmankhattak796@gmail.com>
2025-02-17 13:14:17 +0500

    responsive hero and other section

---
+++ src/app/section/AllProduct.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function AllProduct() {
+  return (
+    <section>
+      <div className="container md:!max-w-full">
+        <div>
+          <h1 className="text-2xl md:text-3xl font-bold">All Products</h1>
+        </div>
+      </div>
+    </section>
+  );
+}
